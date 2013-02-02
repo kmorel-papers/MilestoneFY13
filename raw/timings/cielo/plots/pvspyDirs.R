1a2f0abecd21efb8b86332e35b4de8fee83ac9ed
Ron Oldfield <raoldfi@sandia.gov>
2013-02-01 17:49:09 -0700

    Updated plots and cleaned up some text.

---
+++ raw/timings/cielo/plots/pvspyDirs.R
@@ -0,0 +1,11 @@
+
+insitu_unopt_dirs <- list.files(path="../amr_in-situ-unopt", full.names=TRUE, pattern="pipe-.*", recursive=FALSE)[1:12]
+
+insitu_opt_dirs <- list.files(path="../amr_in-situ-hpctoolkit", full.names=TRUE, pattern="pipe-.*", recursive=FALSE)[1:12]
+
+intransit_dirs <- list.files(path="../amr_in-transit-hpctoolkit", full.names=TRUE, pattern="pipe-.*", recursive=FALSE)[1:12]
+
+intransit_inclusive_dirs <- list.files(path="../amr_in-transit-inclusive", full.names=TRUE, pattern="pipe-.*", recursive=FALSE)[1:12]
+
+spyplot_file_dirs <- list.files(path="../amr-file-hpctoolkit", full.names=TRUE, pattern="pipe-.*", recursive=FALSE)[1:12]
+
